2b6e56281c5cb52b1fe6bdb5fe7d0d0cc4876852
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2025-02-14 16:17:37 +0100

    test(user): adding business tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "e-mails must be always lowercase" do 
+    user = User.new(email_address: "MAIL@EXAMPLE.ORG")
+
+    assert user.email_address == "mail@example.org"
+  end
 end
